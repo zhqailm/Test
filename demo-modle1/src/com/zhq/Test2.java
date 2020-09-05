235ffe13ba7bb5531fc4a9d1f5237628335f7712
zhqailm <1398326910@qq.com>
2020-09-05 14:16:47 +0800

    master修改1

--- demo-modle1/src/com/zhq/Test2.java
+++ demo-modle1/src/com/zhq/Test2.java
@@ -4,6 +4,6 @@ public class Test2 {
     public static void main (String [] args){
         System.out.println("hello");
 
-        System.out.println("hello master");
+        System.out.println("hello master1");
     }
 }
